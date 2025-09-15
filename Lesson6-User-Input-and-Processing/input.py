486efbb0fd71fec9d596af4093be6c5a2d0dd7d4
emrgem <emrgem@bergen.org>
2025-09-15 11:27:46 -0400

    beginning of lesson 6

---
+++ Lesson6-User-Input-and-Processing/input.py
@@ -0,0 +1,27 @@
+# LESSON 6: USER INPUT & PROCESSING - STARTER CODE
+# Follow along with your teacher!
+
+# PART 1: Basic Input
+print("\n--- PART 1: Basic Input ---")
+# Try getting user's name and printing it
+name = input("")
+
+# Try getting a number and adding 1 (this will cause an error!)
+
+
+# PART 2: Type Conversion  
+print("\n--- PART 2: Type Conversion ---")
+# Fix the number problem using int()
+
+
+# Try different conversion functions
+
+
+# PART 3: Math Operators
+print("\n--- PART 3: Math Operators ---")
+# Test all the operators: +, -, *, /, //, **, %
+
+
+# PART 4: Operator Precedence
+print("\n--- PART 4: Operator Precedence ---")
+# Calculate: 9 + 6 / 3 * 2 - 1
